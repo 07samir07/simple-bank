261753315ee9dd3c7834635e89376a8e1fda9b0a
Samir Birua <samirbirua6738@gmail.com>
2026-07-11 08:05:49 +0000

    feat: implement account creation and server setup

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	db "github.com/07samir07/bank-app/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+//Server serves HTTP requests for out banking service
+type Server struct{
+	store *db.Store
+	router *gin.Engine
+}
+
+//NewServer creates a new httpp server and setup routing
+func NewServer(store *db.Store) *Server{
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	server.router = router
+	return server
+}
+
+//Start runs the http server on a specific address
+func(server *Server) Start(address string) error{
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H{
+	return gin.H{"error" : err.Error()}
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/07samir07/bank-app/api"
+	db "github.com/07samir07/bank-app/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:mysecret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+func main(){
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil{
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil{
+		log.Fatal("cannot start server")
+	}
+}
