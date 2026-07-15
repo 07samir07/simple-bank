e06fbd058ee0ef4315064c053ca5155bf1e6910f
Samir Birua <samirbirua6738@gmail.com>
2026-07-15 15:21:27 +0000

    feat: implement currency validation and transfer functionality with associated request handling

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/07samir07/simple-bank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 //Server serves HTTP requests for out banking service
@@ -16,11 +18,19 @@ func NewServer(store db.Store) *Server{
 	server := &Server{store: store}
 	router := gin.Default()
 
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id",server.getAccount)
 	router.GET("/accounts",server.listAccount)
 	router.PUT("/accounts/:id", server.updateAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
+
+
+	router.POST("/transfers", server.createTransfer)
 	server.router = router
 	return server
 }
