271d96ef8e39416b56b4a6d9c2684be1b0beb88a
Samir Birua <samirbirua6738@gmail.com>
2026-08-18 17:28:19 +0000

    feat: update createUser response structure to include additional fields

--- api/user.go
+++ api/user.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	db "github.com/07samir07/simple-bank/db/sqlc"
 	"github.com/07samir07/simple-bank/db/util"
@@ -16,6 +17,16 @@ type createUserRequest struct{
 	FullName string `json:"full_name" binding:"required"`
 	Email string `json:"email" binding:"required,email"`
 }
+
+
+type createUserResponse struct{
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
 func (server *Server) createUser(ctx *gin.Context){
 	var req createUserRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil{
@@ -47,7 +58,15 @@ func (server *Server) createUser(ctx *gin.Context){
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	ctx.JSON(http.StatusOK, user)
+
+	rsp := createUserResponse{
+		Username: user.Username,
+		FullName: user.FullName,
+		Email: user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt: user.CreatedAt,
+	}
+	ctx.JSON(http.StatusOK, rsp)
 }
 
 
