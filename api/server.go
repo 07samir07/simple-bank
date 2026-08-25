7da4755e37e7a7af62ff113390b7fa13a1a6f602
Samir Birua <samirbirua6738@gmail.com>
2026-08-25 16:39:55 +0000

    feat: enhance server initialization with token configuration and update tests

--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/07samir07/simple-bank/db/sqlc"
+	"github.com/07samir07/simple-bank/db/util"
+	"github.com/07samir07/simple-bank/token"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,13 +13,23 @@ import (
 
 //Server serves HTTP requests for out banking service
 type Server struct{
+	config util.Config
 	store db.Store
+	tokenMaker token.Maker
 	router *gin.Engine
 }
 
 //NewServer creates a new httpp server and setup routing
-func NewServer(store db.Store) *Server{
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error){
+	tokenMaker, err := token.NewPasetoMaker("")
+	if err != nil{
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config: config,
+		store: store,
+		tokenMaker : tokenMaker,
+	}
 	router := gin.Default()
 
 
@@ -33,7 +47,7 @@ func NewServer(store db.Store) *Server{
 
 	router.POST("/transfers", server.createTransfer)
 	server.router = router
-	return server
+	return server, nil
 }
 
 //Start runs the http server on a specific address
