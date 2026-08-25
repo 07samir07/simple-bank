ab2efdc27456333b7254b420b512a2338b5927b3
Samir Birua <samirbirua6738@gmail.com>
2026-08-25 17:08:35 +0000

    refactor: reorganize server initialization and setupRouter method

--- api/server.go
+++ api/server.go
@@ -30,24 +30,32 @@ func NewServer(config util.Config, store db.Store) (*Server, error){
 		store: store,
 		tokenMaker : tokenMaker,
 	}
-	router := gin.Default()
+	
 
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+
+
+
+	
+	server.setupRouter()
+	return server, nil
+}
+
+func(server *Server) setupRouter(){
+	router := gin.Default()
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id",server.getAccount)
 	router.GET("/accounts",server.listAccount)
 	router.PUT("/accounts/:id", server.updateAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
-
-
 	router.POST("/transfers", server.createTransfer)
 	server.router = router
-	return server, nil
 }
 
 //Start runs the http server on a specific address
