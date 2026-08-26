28018e783c5594c91346a522f93733c7a3f6f4f8
Samir Birua <samirbirua6738@gmail.com>
2026-08-26 02:25:00 +0000

    feat: implement account ownership verification and update SQL queries for account listing

--- api/account.go
+++ api/account.go
@@ -2,14 +2,16 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/07samir07/simple-bank/db/sqlc"
+	"github.com/07samir07/simple-bank/token"
 	"github.com/gin-gonic/gin"
 )
 
 type createAccountRequest struct{
-	Owner string `json:"owner" binding:"required"` 
+	//Owner string `json:"owner" binding:"required"` 
 	Currency string `json:"currency" binding:"required,currency"`
 }
 func (server *Server) createAccount(ctx *gin.Context){
@@ -19,8 +21,9 @@ func (server *Server) createAccount(ctx *gin.Context){
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.CreateAccountParams{
-		Owner: req.Owner,
+		Owner: authPayload.Username,
 		Currency: req.Currency,
 		Balance: 0,
 	}
@@ -53,6 +56,11 @@ func (server *Server) getAccount(ctx *gin.Context){
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err)) 
+	}
 
 
 	ctx.JSON(http.StatusOK, account)
@@ -68,8 +76,9 @@ func (server *Server) listAccount(ctx *gin.Context){
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.ListAccountsParams{
+		Owner: authPayload.Username,
 		Limit: req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
